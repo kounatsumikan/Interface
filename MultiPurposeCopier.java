95978d33401ce5dcc3c68727d3cf425d2fffe5d9
kounatsumikan <ko_natsuhara@albert2005.co.jp>
2018-08-21 11:58:53 +0900

    インターフェースの実装

--- MultiPurposeCopier.java
+++ MultiPurposeCopier.java
@@ -1,16 +1,27 @@
-public class MultiPurposeCopier extends Copier{
+public class MultiPurposeCopier extends Copier implements Printer,Fax,Scanner{
 
     // コピー機の機能
     //public Paper copy(Paper origin)
 
-    /*
+    private Paper paper1;
+    private Document document1;
     // スキャナの機能
-    public Document scan(Paper document)
+    public Document scan(Paper document){
+        paper1 = document;
+        return 0;
+    }
 
     // ファクシミリの機能
-    public Paper receive()
-    public void send(Paper document, String to)
+    public Paper receive(){
+        return 0;
+    }
+    public void send(Paper document, String to){
+
+    }
 
     // プリンタの機能
-    public Paper print(Document document)*/
+    public Paper print(Document document){
+        document1 = document;
+        return 0;
+    }
 }
