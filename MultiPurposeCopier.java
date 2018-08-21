78aedf0a4fb60cbe50d5839d868b0169322643fd
kounatsumikan <ko_natsuhara@albert2005.co.jp>
2018-08-21 11:15:22 +0900

    Copierを継承したMultiPurposeCopierの作成

---
+++ MultiPurposeCopier.java
@@ -0,0 +1,15 @@
+public class MultiPurposeCopier extends{
+
+    // コピー機の機能
+    public Paper copy(Paper origin)
+    /*
+    // スキャナの機能
+    public Document scan(Paper document)
+
+    // ファクシミリの機能
+    public Paper receive()
+    public void send(Paper document, String to)
+
+    // プリンタの機能
+    public Paper print(Document document)*/
+}
